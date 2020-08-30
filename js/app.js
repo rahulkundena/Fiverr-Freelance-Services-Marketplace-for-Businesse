bf51c5437302a379aa6e316f2119ab14887e1e4c
Kundena Rahul <55629530+rahulkundena@users.noreply.github.com>
2020-08-30 20:19:17 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+window.onscroll = function(e){
+    let scroll = window.scrollY;
+
+    const headerScroll = document.querySelector('.header-scroll');
+    if (scroll >300){
+        headerScroll.classList.add('active');
+    }
+    else{
+        headerScroll.classList.remove('active');
+    }
+}
